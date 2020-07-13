caefe9ca3525d160ecb2acca7403dbd3d9429704
Terence <TerenceFrade@gmail.com>
2020-07-13 16:37:30 +0200

    Backend setup with swashbuckle and DB context

---
+++ Questionnaire_BackEnd/Questionnaire_BackEnd/Data/QuestionnaireContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Questionnaire_BackEnd.Data
+{
+    public class QuestionnaireContext : DbContext
+    {
+        public QuestionnaireContext(DbContextOptions<QuestionnaireContext> options) : base(options)
+        {
+        }
+    }
+}
