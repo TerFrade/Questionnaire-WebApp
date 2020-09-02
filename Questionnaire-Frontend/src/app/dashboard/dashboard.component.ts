9c697ada3ec3740ec848af1993a9b3e577fb1952
Terence <TerenceFrade@gmail.com>
2020-09-02 20:15:02 +0200

    created frontend and setup basic rules and dashboard.

---
+++ Questionnaire-Frontend/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: "dashboard",
+  templateUrl: "./dashboard.component.html",
+  styles: ["./dashboard.component.scss"]
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
