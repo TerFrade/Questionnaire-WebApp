eb062fcc4d070443fd617bed82386b161ac13afa
Terence Frade <terencefrade@gmail.com>
2021-02-19 14:58:47 +0200

    Navbar, Home Page layout Started Navbar, Home Page Status: WIP.

--- Questionnaire-Frontend/src/app/app.component.ts
+++ Questionnaire-Frontend/src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `<navbar></navbar>
-    <router-outlet>
-      <home></home>
-    </router-outlet>`,
+  template: ` <navbar></navbar>
+    <main role="main">
+      <!-- style="padding-top: 5rem;" - for fixed top -->
+      <router-outlet><home></home></router-outlet>
+    </main>
+    <footer class="container footer text-muted">
+      <p>&copy; Questionnaire.io 2021 - Terence Frade Project</p>
+    </footer>`,
   styles: [],
 })
 export class AppComponent {
