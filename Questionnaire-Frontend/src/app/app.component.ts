b1a1c3d0c6436041987223a35c2e12f13f291f53
Terence Frade <terencefrade@gmail.com>
2021-02-27 07:42:11 +0200

    Questionnaire Page Styling Done.

--- Questionnaire-Frontend/src/app/app.component.ts
+++ Questionnaire-Frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: ` <navbar></navbar>
-    <main role="main">
-      <!-- style="padding-top: 5rem;" - for fixed top -->
+    <main role="main" class="container pt-4">
       <router-outlet></router-outlet>
+      <hr class="mt-1" />
     </main>
+
     <footer class="container footer text-muted">
       <p>&copy; Questionnaire.io 2021 - Terence Frade Project</p>
     </footer>`,
